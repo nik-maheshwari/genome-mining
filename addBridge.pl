5a997c3c7e47d779728e80c89e163837eeecb679
Dr. Nikunj Maheshwari <nikunj.maheshwari@ucdconnect.ie>
2018-04-23 14:41:17 +0100

    Update addBridge.pl

--- addBridge.pl
+++ addBridge.pl
@@ -3,5 +3,5 @@ use warnings;
 
 use lib "./"; #change this path to location of Bridge.pm
 use Bridge qw(&brid);
-my ($bridge,@pos,%temp1)= &brid("MNKLNSNAVVSLNEVSDSELDTILGGNRWWQGVVPTVSYECRMNSWQHVFTCC");
+my ($bridge,@pos,%temp1)= &brid("MNKLNSNAVVSLNEVSDSELDTILGGNRWWQGVVPTVSYECRMNSWQHVFTCC"); #Replace the peptide with your peptide of interest
 print $bridge;
