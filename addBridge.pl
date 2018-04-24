1006b104fe8070ba22b5f0114a6c1918b0ff56c0
Dr. Nik <nikunj.maheshwari@ucdconnect.ie>
2018-04-24 15:43:47 +0100

    Update addBridge.pl
    Input and output are files.

--- addBridge.pl
+++ addBridge.pl
@@ -1,7 +1,18 @@
 use strict;
 use warnings;
-
+use Bio::SeqIO;
 use lib "./"; #change this path to location of Bridge.pm
 use Bridge qw(&brid);
-my ($bridge,@pos,%temp1)= &brid("MNKLNSNAVVSLNEVSDSELDTILGGNRWWQGVVPTVSYECRMNSWQHVFTCC"); #Replace the peptide with your peptide of interest
-print $bridge;
+
+open my $out, ">peptide_bridge.txt" or die;
+print $out "ID\tSequence\tBridge\n";
+my $io=Bio::SeqIO->new(-file=>"peptide.fasta",'-format'=>'Fasta'); #Peptides in FASTA format
+while(my $pep=$io->next_seq)
+{
+	my $id=$pep->id;
+	my $seq=$pep->seq;
+	my ($bridge,@pos,%temp1)= &brid($seq);
+	print $out $id."\t".$seq."\t".$bridge."\n";
+}
+
+close $out;
